1acd632b134fe107b83426b1e9283f1db6e0b857
Keven-1OO <kevenviciadogamer@gmail.com>
2025-02-22 17:35:42 +0100

    FrontYBack5.0

---
+++ _PaginaWeb_ComparadorPreciosFront/scripts/buscador.js
@@ -0,0 +1,173 @@
+// // ------------------------------ --------SCRIPT AXIOS-----------------------------------------------------
+// async function getProductos() {
+//     let productos = await axios.get("http://localhost:8084/productos/todos/");
+//     console.log("los productos: ", productos.data);
+
+//     let Productos = document.getElementById('verProductos');
+//     console.log(Productos);
+
+//     productos.data.forEach(item => {
+//         console.log(item);
+
+//         // Crear un div para cada producto
+//         let divProducto = document.createElement('div');
+//         divProducto.classList.add('producto'); // Agrega una clase para estilos
+
+//         // Crear div para la imagen
+//         let divImagen = document.createElement('div');
+//         let imagen = document.createElement('img');
+//         imagen.src = `../imagenes/${item.nombre}.jpg`; // Ruta dinámica
+//         imagen.alt = item.nombre; // Texto alternativo
+//         divImagen.appendChild(imagen);
+//         divProducto.appendChild(divImagen);
+
+//         // Crear divs para cada dato del producto
+//         let divCategoria = document.createElement('div');
+//         divCategoria.textContent = item.categorias.nombre;
+//         divCategoria.classList.add('Categoria');
+
+//         divProducto.appendChild(divCategoria);
+
+//         let divNombre = document.createElement('div');
+//         divNombre.textContent = item.nombre;
+//         divNombre.classList.add('Nombre');
+
+//         divProducto.appendChild(divNombre);
+
+//         let divMarca = document.createElement('div');
+//         divMarca.textContent = 'Marca: ' + item.marca;
+//         divMarca.classList.add('Marca');
+
+//         divProducto.appendChild(divMarca);
+
+//         let divPrecio = document.createElement('div');
+//         divPrecio.textContent = 'Desde: ' + item.precioVenta + '€';
+//         divPrecio.classList.add('Precio');
+
+//         divProducto.appendChild(divPrecio);
+
+//         let divOfertas = document.createElement('div');
+//         divOfertas.innerHTML = '<button onclick="verOfertas(' + item.idProducto + ')">Ver Ofertas</button>'; // Botón con HTML
+//         divOfertas.classList.add('btnVerOfertas')
+
+//         divProducto.appendChild(divOfertas);
+
+//         // Agregar el div del producto al contenedor principal
+//         Productos.appendChild(divProducto);
+//     });
+// }
+
+document.getElementById('searchForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const searchTerm = document.getElementById('productIdInput').value;
+    const searchType = document.getElementById('searchType').value;
+
+    if (searchTerm) {
+        let producto;
+        if (searchType === 'id') {
+            producto = await getBuscarProducto(searchTerm);
+        } else if (searchType === 'nombre') {
+            producto = await getBuscarProductoPorNombre(searchTerm);
+        }
+
+        if (producto) {
+            console.log("Producto encontrado:", producto);
+            mostrarProducto(producto);
+        } else {
+            console.log("Producto no encontrado");
+            document.getElementById('resultadoBusqueda').textContent = "Producto no encontrado.";
+        }
+    } else {
+        console.log("Por favor, ingrese un término de búsqueda.");
+        document.getElementById('resultadoBusqueda').textContent = "Por favor, ingrese un término de búsqueda.";
+    }
+});
+
+document.getElementById('toggleSearchType').addEventListener('click', function () {
+    const searchTypeInput = document.getElementById('searchType');
+    const inputField = document.getElementById('productIdInput');
+    if (searchTypeInput.value === 'id') {
+        searchTypeInput.value = 'nombre';
+        inputField.placeholder = 'Buscar por Nombre';
+        this.textContent = 'Buscar por ID';
+    } else {
+        searchTypeInput.value = 'id';
+        inputField.placeholder = 'Buscar por ID';
+        this.textContent = 'Buscar por Nombre';
+    }
+});
+
+async function getBuscarProducto(id) {
+    try {
+        const response = await axios.get(`http://localhost:8084/productos/buscar/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error("Producto no encontrado o código de barras incorrecto:", error);
+        return null;
+    }
+}
+
+async function getBuscarProductoPorNombre(nombre) {
+    try {
+        const response = await axios.get(`http://localhost:8084/productos/buscarPorNombre/${nombre}`);
+        return response.data;
+    } catch (error) {
+        console.error("Producto no encontrado o nombre incorrecto:", error);
+        return null;
+    }
+}
+
+function mostrarProducto(item) {
+    const Productos = document.getElementById('resultadoBusqueda'); // Usamos el div resultadoBusqueda como contenedor
+
+    // Limpiar resultados anteriores
+    Productos.innerHTML = '';
+
+    // Crear un div para cada producto
+    let divProducto = document.createElement('div');
+    divProducto.classList.add('producto'); // Agrega una clase para estilos
+
+    // Crear div para la imagen
+    let divImagen = document.createElement('div');
+    let imagen = document.createElement('img');
+    imagen.src = `../imagenes/${item.nombre}.jpg`; // Ruta dinámica
+    imagen.alt = item.nombre; // Texto alternativo
+    divImagen.appendChild(imagen);
+    divProducto.appendChild(divImagen);
+
+    // Crear divs para cada dato del producto
+    let divCategoria = document.createElement('div');
+    divCategoria.textContent = item.categorias.nombre; // Ajuste para categoria.nombre
+    divCategoria.classList.add('Categoria');
+
+    divProducto.appendChild(divCategoria);
+
+    let divNombre = document.createElement('div');
+    divNombre.textContent = item.nombre;
+    divNombre.classList.add('Nombre');
+
+    divProducto.appendChild(divNombre);
+
+    let divMarca = document.createElement('div');
+    divMarca.textContent = 'Marca: ' + item.marca;
+    divMarca.classList.add('Marca');
+
+    divProducto.appendChild(divMarca);
+
+    let divPrecio = document.createElement('div');
+    divPrecio.textContent = 'Desde: ' + item.precioVenta + '€'; // Ajuste para precio_venta
+    divPrecio.classList.add('Precio');
+
+    divProducto.appendChild(divPrecio);
+
+    let divOfertas = document.createElement('div');
+    divOfertas.innerHTML = `<button onclick="verOfertas(${item.idProducto})">Ver Ofertas</button>`; // Botón con HTML
+    divOfertas.classList.add('btnVerOfertas')
+
+    divProducto.appendChild(divOfertas);
+
+    // Agregar el div del producto al contenedor principal
+    Productos.appendChild(divProducto);
+}
+// getProductos();
--- _PaginaWeb_ComparadorPreciosFront/scripts/precioProvedores.js
+++ _PaginaWeb_ComparadorPreciosFront/scripts/precioProvedores.js
@@ -1,58 +1,117 @@
-console.log("local: ", localStorage.getItem("id"));
-async function getPrecioProvedores() {
-    let productos = await axios.get(`http://localhost:8084/productos/precios/provedor/${localStorage.getItem("id")}`);
-    console.log("los productos: ", productos.data);
-
-    let tablaProductos = document.getElementById('tablaPrecioProductos');
-    console.log(tablaProductos);
-
-    // Crear la estructura de la tabla fuera del bucle
-    let tabla = document.createElement('table');
-    let thead = tabla.insertRow();
-    thead.insertCell().textContent = "Nombre";
-    thead.insertCell().textContent = "Precio proveedor";
-    thead.insertCell().textContent = "Empresa";
-
-    let tbody = tabla.appendChild(document.createElement('tbody'));
-
-    productos.data.forEach(item => {
-        console.log(item);
-
-        // Crear una nueva fila para cada producto
-        let row = tbody.insertRow();
-        row.insertCell().textContent = item.producto.nombre;
-        row.insertCell().textContent = item.precioProvedor + "€";
-        row.insertCell().textContent = item.provedor.empresa;
-
-    });
-
-    // Agregar la tabla completa al DOM una vez que se han agregado todas las filas
-    tablaProductos.appendChild(tabla);
-
-
-}
-
-// async function getProductoDetalle() {
+// console.log("local: ", localStorage.getItem("id"));
+// async function getPrecioProvedores() {
 //     let productos = await axios.get(`http://localhost:8084/productos/precios/provedor/${localStorage.getItem("id")}`);
 //     console.log("los productos: ", productos.data);
 
-//     let productoDetale = document.getElementById('ProductoDetalle');
-//     console.log(productoDetale);
+//     let tablaProductos = document.getElementById('tablaPrecioProductos');
+//     console.log(tablaProductos);
+
+//     // Crear la estructura de la tabla fuera del bucle
+//     let tabla = document.createElement('table');
+//     let thead = tabla.insertRow();
+//     thead.insertCell().textContent = "Nombre";
+//     thead.insertCell().textContent = "Precio proveedor";
+//     thead.insertCell().textContent = "Empresa";
+
+//     let tbody = tabla.appendChild(document.createElement('tbody'));
 
 //     productos.data.forEach(item => {
 //         console.log(item);
-// // Crear div para la imagen
-// let divImagen = document.createElement('div');
-// divImagen.classList.add=('imagenProducto');
-// // let imagen = document.createElement('img');
-// // imagen.src = `../imagenes/${item.nombre}.jpg`; // Ruta dinámica
-// // imagen.alt = item.nombre; // Texto alternativo
 
-//         divImagen.appendChild(imagen);
+//         // Crear una nueva fila para cada producto
+//         let row = tbody.insertRow();
+//         row.insertCell().textContent = item.producto.nombre;
+//         row.insertCell().textContent = item.precioProvedor + "€";
+//         row.insertCell().textContent = item.provedor.empresa;
+
 //     });
+
+//     // Agregar la tabla completa al DOM una vez que se han agregado todas las filas
+//     tablaProductos.appendChild(tabla);
 
 
 // }
 
+// getPrecioProvedores();
+console.log("local: ", localStorage.getItem("id"));
+
+async function getPrecioProvedores() {
+    try {
+        mostrarIndicadorCarga(); // Mostrar indicador de carga
+
+        const productos = await axios.get(`http://localhost:8084/productos/precios/provedor/${localStorage.getItem("id")}`);
+        console.log("los productos: ", productos.data);
+
+        ocultarIndicadorCarga(); // Ocultar indicador de carga
+
+        if (!productos.data || productos.data.length === 0) {
+            mostrarMensaje("No se encontraron precios para este producto.");
+            return;
+        }
+
+        const contenedorPrecios = document.getElementById('tablaPrecioProductos');
+        contenedorPrecios.innerHTML = ''; // Limpiar contenido anterior
+
+        productos.data.forEach(item => {
+            const tarjetaPrecio = crearTarjetaPrecio(item);
+            contenedorPrecios.appendChild(tarjetaPrecio);
+        });
+    } catch (error) {
+        console.error("Error al obtener precios:", error);
+        ocultarIndicadorCarga();
+        mostrarMensaje("Error al obtener precios. Inténtalo de nuevo más tarde.");
+    }
+}
+
+function crearTarjetaPrecio(item) {
+    const tarjeta = document.createElement('div');
+    tarjeta.classList.add('tarjeta-precio');
+
+    const nombre = document.createElement('h3');
+    nombre.textContent = item.producto.nombre;
+    tarjeta.appendChild(nombre);
+
+    const precio = document.createElement('div');
+    precio.classList.add('precio');
+    precio.textContent = formatearPrecio(item.precioProvedor);
+    tarjeta.appendChild(precio);
+
+    const empresa = document.createElement('div');
+    empresa.classList.add('empresa');
+    empresa.textContent = item.provedor.empresa;
+    tarjeta.appendChild(empresa);
+
+    const botonVerOferta = document.createElement('button');
+    botonVerOferta.textContent = "Ver oferta";
+    tarjeta.appendChild(botonVerOferta);
+
+    return tarjeta;
+}
+
+function formatearPrecio(precio) {
+    return new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'EUR' }).format(precio);
+}
+
+function mostrarIndicadorCarga() {
+    const indicadorCarga = document.createElement('div');
+    indicadorCarga.textContent = "Cargando precios...";
+    indicadorCarga.id = "indicadorCarga";
+    document.getElementById('tablaPrecioProductos').appendChild(indicadorCarga);
+}
+
+function ocultarIndicadorCarga() {
+    const indicadorCarga = document.getElementById('indicadorCarga');
+    if (indicadorCarga) {
+        indicadorCarga.remove();
+    }
+}
+
+function mostrarMensaje(mensaje) {
+    const mensajeDiv = document.createElement('div');
+    mensajeDiv.textContent = mensaje;
+    mensajeDiv.classList.add('mensaje');
+    document.getElementById('tablaPrecioProductos').appendChild(mensajeDiv);
+}
+// ------------------------------- DETALLE PRODUCTO
+
 getPrecioProvedores();
-// getProductoDetalle();
